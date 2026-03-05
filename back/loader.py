845381eca2ebb8a76a2a20965d0e2bb473ca2702
Mahay <hirimanana@yahoo.fr>
2026-03-05 20:02:39 +0400

    update loader

--- back/loader.py
+++ back/loader.py
@@ -1,3 +1,20 @@
-def load_document(path: str) -> str:
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
+import os
+
+DOCUMENT_FOLDER = "data"
+
+def load_documents():
+    documents = []
+
+    for filename in os.listdir(DOCUMENT_FOLDER):
+        if filename.endswith(".txt"):
+            path = os.path.join(DOCUMENT_FOLDER, filename)
+
+            with open(path, "r", encoding="utf-8") as f:
+                text = f.read()
+
+            documents.append({
+                "source": filename,
+                "content": text
+            })
+
+    return documents
